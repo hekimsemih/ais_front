60025f845dbc8a9aa3098c4570d937d9028a3ba4
Olivier G-R <olivier@fractalwire.io>
2020-07-07 14:19:45 +0200

    serve shaders folder as static files

--- lib/ais_front_web/endpoint.ex
+++ lib/ais_front_web/endpoint.ex
@@ -24,7 +24,7 @@ defmodule AisFrontWeb.Endpoint do
     at: "/",
     from: :ais_front,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js shaders favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
