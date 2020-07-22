bf645a90c24d73c58500d1e5d17303cb720ba8bc
Olivier G-R <olivier@fractalwire.io>
2020-07-22 16:25:35 +0200

    add timex module

--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule AisFront.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:timex, "~> 3.5"}
     ]
   end
 
