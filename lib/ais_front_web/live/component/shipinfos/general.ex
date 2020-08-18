89c4a68a96acc582e61b1bc062ef4d1b5a35eaaa
olivier G-R <olivier@fractalwire.io>
2020-08-18 18:51:52 +0200

    Shipinfos description with more details

--- lib/ais_front_web/live/component/shipinfos/general.ex
+++ lib/ais_front_web/live/component/shipinfos/general.ex
@@ -5,10 +5,18 @@ defmodule AisFrontWeb.Live.Component.ShipInfos.General do
 
   def render(%{shipinfos: _shipinfos} = assigns) do
     ~L"""
-    <div class="shipinfos-description">
+    <div id="shipinfos-description">
       <p>
-        <strong><%= ShipInfos.pretty_date!(@shipinfos) %></strong>, the vessel 
-        <strong><%= ShipInfos.pretty_name!(@shipinfos) %></strong> was located at
+        <strong><%= ShipInfos.pretty_date!(@shipinfos) %></strong>, the vessel
+        <%= if @shipinfos.name != "" do %>
+          known as <strong><%= @shipinfos.name %></strong>
+        <% end %>
+        identified with
+        <%= if @shipinfos.callsign != "" do %>
+          radio callsign <strong><%= @shipinfos.callsign %></strong> and
+        <% end %>
+          mmsi number <strong><%= @shipinfos.mmsi %></strong>
+        was located at
         <strong><%= ShipInfos.pretty_point!(@shipinfos, unit: :dms, compass?: true) %></strong>
       </p>
     </div>
