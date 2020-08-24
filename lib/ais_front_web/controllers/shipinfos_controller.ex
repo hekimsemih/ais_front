1b8538215da24d17548a12ee856445e541f12900
olivier G-R <olivier@fractalwire.io>
2020-08-24 18:16:39 +0200

    scratch shipinfos controller + view

--- lib/ais_front_web/controllers/shipinfos_controller.ex
+++ lib/ais_front_web/controllers/shipinfos_controller.ex
@@ -6,38 +6,53 @@ defmodule AisFrontWeb.ShipinfosController do
 
   action_fallback AisFrontWeb.FallbackController
 
-  def index(conn, _params) do
-    shipinfos = Core.list_shipinfos()
-    render(conn, "index.json", shipinfos: shipinfos)
-  end
-
-  def create(conn, %{"shipinfos" => shipinfos_params}) do
-    with {:ok, %Shipinfos{} = shipinfos} <- Core.create_shipinfos(shipinfos_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", Routes.shipinfos_path(conn, :show, shipinfos))
-      |> render("show.json", shipinfos: shipinfos)
+  def index(conn, %{"format" => format, "view" => view}) do
+    shipinfos = case view do
+      "full" -> Core.list_shipinfos_with_type
     end
+    render(
+      conn,
+      "#{view}_index.#{format}",
+      shipinfos: shipinfos,
+    )
   end
+  def index(conn, %{}), do: index(conn, %{"format" => "json", "view" => "full"})
 
-  def show(conn, %{"id" => id}) do
-    shipinfos = Core.get_shipinfos!(id)
-    render(conn, "show.json", shipinfos: shipinfos)
-  end
-
-  def update(conn, %{"id" => id, "shipinfos" => shipinfos_params}) do
-    shipinfos = Core.get_shipinfos!(id)
-
-    with {:ok, %Shipinfos{} = shipinfos} <- Core.update_shipinfos(shipinfos, shipinfos_params) do
-      render(conn, "show.json", shipinfos: shipinfos)
+  def show(conn, %{"id" => id, "format" => format, "view" => view}) do
+    shipinfos = case view do
+      "full" -> Core.list_shipinfos_with_type
     end
+    render(
+      conn,
+      "#{view}_show.#{format}",
+      shipinfos: shipinfos
+    )
   end
+  def show(conn, %{"id" => id}), do: index(conn, %{"format" => "json", "view" => "full"})
 
-  def delete(conn, %{"id" => id}) do
-    shipinfos = Core.get_shipinfos!(id)
+  # def create(conn, %{"shipinfos" => shipinfos_params}) do
+  #   with {:ok, %Shipinfos{} = shipinfos} <- Core.create_shipinfos(shipinfos_params) do
+  #     conn
+  #     |> put_status(:created)
+  #     |> put_resp_header("location", Routes.shipinfos_path(conn, :show, shipinfos))
+  #     |> render("show.json", shipinfos: shipinfos)
+  #   end
+  # end
 
-    with {:ok, %Shipinfos{}} <- Core.delete_shipinfos(shipinfos) do
-      send_resp(conn, :no_content, "")
-    end
-  end
+
+  # def update(conn, %{"id" => id, "shipinfos" => shipinfos_params}) do
+  #   shipinfos = Core.get_shipinfos!(id)
+
+  #   with {:ok, %Shipinfos{} = shipinfos} <- Core.update_shipinfos(shipinfos, shipinfos_params) do
+  #     render(conn, "show.json", shipinfos: shipinfos)
+  #   end
+  # end
+
+  # def delete(conn, %{"id" => id}) do
+  #   shipinfos = Core.get_shipinfos!(id)
+
+  #   with {:ok, %Shipinfos{}} <- Core.delete_shipinfos(shipinfos) do
+  #     send_resp(conn, :no_content, "")
+  #   end
+  # end
 end
--- lib/ais_front_web/views/shipinfos_view.ex
+++ lib/ais_front_web/views/shipinfos_view.ex
@@ -4,12 +4,12 @@ defmodule AisFrontWeb.ShipinfosView do
 
   alias AisFront.Core.Shipinfos
 
-  def render("index.json", %{shipinfos: shipinfos}) do
-    %{data: render_many(shipinfos, ShipinfosView, "shipinfos.json")}
+  def render("index.json", %{shipinfos_with_type: shipinfos_with_type, format: format, view: view}) do
+    %{data: render_many(shipinfos_with_type, ShipinfosView, "shipinfos.json", as: :shipinfos_with_type, format: format, view: view)}
   end
 
-  def render("show.json", %{shipinfos: shipinfos}) do
-    %{data: render_one(shipinfos, ShipinfosView, "shipinfos.json")}
+  def render("show.json", %{shipinfos_with_type: shipinfos_with_type}) do
+    %{data: render_one(shipinfos_with_type, ShipinfosView, "shipinfos.json")}
   end
 
   defp shipinfos_description(shipinfos) do
@@ -17,24 +17,27 @@ defmodule AisFrontWeb.ShipinfosView do
   end
   defp shipinfos_heading_or_cog(shipinfos) do
     cond do
-      heading != 511 -> heading
-      cog >= 360.0 -> 0.0
-      true -> cog
+      shipinfos.heading != 511 -> shipinfos.heading
+      shipinfos.cog >= 360.0 -> 0.0
+      true -> shipinfos.cog
     end
   end
-  @aismap_properties [:mmsi, ]
-  def render("shipinfos.json", %{shipinfos: shipinfos}) do
-    keys = Map.keys(value)
-           |> Enum.filter(& &1 in @aismap_properties)
-    Map.take(value, keys)
+
+  # @aismap_properties [:mmsi, ]
+  def render("shipinfos.json", %{shipinfos_with_type: shipinfos_with_type, format: format, view: view}) do
+    # keys = Map.keys(value)
+    #        |> Enum.filter(& &1 in @aismap_properties)
+    # Map.take(value, keys)
+    {shipinfos, shiptype} = shipinfos_with_type
+    IO.inspect(shipinfos.point)
+    p = Geo.JSON.encode!(shipinfos.point)
 
     %{
       type: "Feature",
       id: shipinfos.mmsi,
-      geometry: Geo.JSON.encode!(shipinfos.point),
+      geometry: p,
       geometry_name: "point",
-      properties:
+      properties: ""
     }
-    %{mmsi: shipinfos.mmsi}
   end
 end
